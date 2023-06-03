67710ded84c3dfaa45c4bc51ff61016ea9b3f734
Haznto <haznto@gmail.com>
2023-06-03 20:03:44 +0000

    adding endpoints for the new page

--- schema.sql
+++ schema.sql
@@ -12,3 +12,13 @@ create table if not exists user_contact_form(
   category_url varchar(10000),
   description varchar(100000)
   );
+
+  create table if not exists user_schedule(
+  id serial primary key,
+  bodypart varchar (255),
+  equipment varchar(10000),
+  gifUrl varchar(100000),
+  exercise_name varchar(100000),
+  exercise_target varchar(100000),
+  week_day varchar(255)
+  );
